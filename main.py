a92dd2cec6435cfda5ee23fe32bb0f5f8f9fec33
mbookamer <79340162+mbookamer@users.noreply.github.com>
2023-04-27 22:18:32 -0500

    Update main.py
    added all functions and commented

--- main.py
+++ main.py
@@ -13,6 +13,37 @@ def shortest_shortest_path(graph, source):
       (shortest path weight, shortest path number of edges). See test case for example.
     """
     ### TODO
+    
+    #initialize variables
+    distances = {vertex: (float('inf'), float('inf')) for vertex in graph}
+    paths = {vertex: [] for vertex in graph} #we want to store all paths in a single place
+    
+    #we want to have a single starting vertex to compute paths so we initialize it
+    distances[source] = (0,0)
+    paths[source] = [source]
+    
+    #we want to create a queue of all verticies we want to visit and as we visit them, pop them 
+    #off so we do not revisit them
+    queue = [source]
+    while queue:
+        #now we want to find the shortest distance from a vertex to the queue
+        vertex = min(queue, key=lambda v: distances[v])
+        queue.remove(vertex)
+        
+    for adjacent, weight in graph[vertex]: 
+        #want to compute the distances and paths of adjacent verticesm so we go over all possibilities 
+        if distances[adjacent][0] > distances[vertex][0] + weight:
+            distances[adjacent] = (distances[vertex][0] + weight, distances[vertex][1] +1)
+            paths[adjacent] = paths[vertex] + [adjacent]
+        elif distances[adjacent][0] == distances[vertex][0] + weight:
+            if distances[adjacent][1] > distances[vertex][1] + 1:
+                distances[adjacent] = (distances[vertex][0] + weight, distances[vertex][1] +1)
+                paths[adjacent] = paths[vertex] + [adjacent]
+                if adjacent not in queue:
+                    queue.append(adjacent)
+    #then we want to return all of the distances between the starting vertext and adjacent vertices that we computed
+    return distances
+            
     pass
     
 def test_shortest_shortest_path():
@@ -41,6 +72,24 @@ def bfs_path(graph, source):
       that vertex in the shortest path tree.
     """
     ###TODO
+    #want to initialize a parent dictionary to use
+    parents = {vertex: None for vertex in graph}
+    
+    #want the starting vertex of dict to point to itself
+    parents[source] = source
+    
+    #like before we want to create a queue of vertices to visit
+    #want to pop off every node that we visit and add vertices not in the queue to it
+    queue = [source]
+    while queue:
+        vertex = queue.pop(0)
+    #we want to update the parent of the adjacent vertices
+    for adjacent in graph[vertex]:
+        if parents[adjacent] is None: #if the adjacent vertex is not in our queue to visit
+            parents[adjacent] = vertex
+            queue.append(adjacent)
+            
+    return parents
     pass
 
 def get_sample_graph():
@@ -66,6 +115,26 @@ def get_path(parents, destination):
       (excluding the destination node itself). See test_get_path for an example.
     """
     ###TODO
+    
+    #initalize array for path
+    path = []
+    for i in parents:
+        if i != parents[i]:
+            #we only want to record vertices in the path we are interested in looking at 
+            if parents[i] in path:
+                continue
+            else: #add it to path if it is not already in the array
+                path.append(parents[i])
+        elif i == destination:
+            path.append(parents[i])
+            break
+        else:
+            continue 
+        return (''.join(list((path)))) #we want to return the full path 
+    
+    graph = get_sample_graph()
+    parents = bfs_path(graph, 's')
+    get_path(parents, 'd')
     pass
 
 def test_get_path():
